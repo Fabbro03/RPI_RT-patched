ac4a9bac538ecc13636e627bfda6305604cc4408
Marco Fabbroni <marco.fabbroni@outlook.it>
2025-09-06 15:47:12 +0200

    Update extraxted image name

--- build.sh
+++ build.sh
@@ -33,7 +33,9 @@ download_image(){
     curl -s https://downloads.raspberrypi.org/${RASPIOS}/images/${RASPIOS}-${DATE}/${RASPIOS_IMAGE_NAME}.xz \
         --output ${RASPIOS}.xz
     xz -d -f ${RASPIOS}.xz
-    mv ${RASPIOS_IMAGE_NAME} ${RASPIOS}.img
+    # Detect the extracted filename
+    IMG_EXTRACTED=$(ls -1 *.img | head -n 1)
+    mv "$IMG_EXTRACTED" ${RASPIOS}.img
 }
 
 download_kernel_src(){
